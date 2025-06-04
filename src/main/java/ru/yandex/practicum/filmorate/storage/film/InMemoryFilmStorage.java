25e7bbda5a86a6cf5a38497ad49b40ebb5c44522
avv <avv37@yandex.com>
2025-06-04 16:43:23 +0300

    Выполнение ТЗ по спринту 11 доработки по замечаниям

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
